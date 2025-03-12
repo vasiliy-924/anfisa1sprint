f839dd46c7aa6b9d5a219fced8caeded86589fa6
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-03-12 22:11:10 +0300

    after mine changes

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('homepage.urls', namespace='homepage')),
-    path('about/', include('about.urls', namespace='about')),
-    path('ice_cream/', include('ice_cream.urls', namespace='ice_cream')),
+    path('', include(('homepage.urls', 'homepage'), namespace='homepage')),
+    path('about/', include(('about.urls', 'about'), namespace='about')),
+    path('ice_cream/', include(('ice_cream.urls', 'ice_cream'), namespace='ice_cream')),
     path('admin/', admin.site.urls),
 ]
